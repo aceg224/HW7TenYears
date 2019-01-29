42cb543e68335c027ecd3f2aa30d6640dbde5750
aceg224 <aceg224@gmail.com>
2019-01-29 08:19:57 +0800

    Initial Commit

---
+++ HW7TenYears/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW7TenYears
+//
+//  Created by min-chia on 2019/1/29.
+//  Copyright © 2019 min-chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
